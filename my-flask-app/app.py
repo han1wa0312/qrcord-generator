15331805c950dc39ff6ade3d40a944c0b44f13bf
han1wa0312 <isigakifrogfish@gmail.com>
2024-12-11 19:29:45 +0900

    Add files via upload

---
+++ my-flask-app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, send_file
+import qrcode
+import io
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate_qr():
+    url = request.form.get('url')
+    qr = qrcode.make(url)
+    img_io = io.BytesIO()
+    qr.save(img_io, 'PNG')
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/png')
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
